f606994e21ee5f680d2153556a9061bd9ac755cb
Scott Gelini <sg00251@srcs.org>
2020-01-31 14:25:36 -0800

    1/31/2020

---
+++ Boardwalk Breakout/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+    public float speed = 7.0f;
+    public float rotationSpeed = 3.0f;
+
+    bool isGrounded;
+    Rigidbody rb;
+    
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Ground"))
+            isGrounded = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Ground"))
+            isGrounded = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(0, -rotationSpeed, 0);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(0, rotationSpeed, 0);
+
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            pos.z += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            pos.z -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+
+        transform.position = pos;
+
+
+
+        
+
+
+    }
+}
+
+
+
+
+
---
+++ Boardwalk Breakout/Assets/Scripts/Plushie1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plushie1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Transform bear;
+    [SerializeField] private Vector3 offset;
+
+    void Start()
+    {
+        bear = GameObject.Find("Player").transform;
+        offset = new Vector3(0, 0, 2);
+    }
+
+    void Update()
+    {
+        transform.position = bear.position + offset;
+        
+    }
+}
+
