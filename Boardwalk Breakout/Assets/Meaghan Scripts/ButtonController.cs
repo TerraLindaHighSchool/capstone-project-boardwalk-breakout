5a936bfd882918fef39332b4d802f8f96b1b628d
mb00126 <meaghanb101@gmail.com>
2020-02-03 14:08:22 -0800

    added more stuff to button

--- Boardwalk Breakout/Assets/Meaghan Scripts/ButtonController.cs
+++ Boardwalk Breakout/Assets/Meaghan Scripts/ButtonController.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class ButtonController : MonoBehaviour
 {
-    public GameObject door;
+    /*
+     * Plushies MUST have the plushie tag. To create a button, create a cylinder w/ rigidbody. Duplicate it. Uncheck mesh renderer, add mess collider. Turn on is convex and is trigger.
+     * Move it by pressing V to align it exactly ontop the cylinder. Add the script. In the inspector, enter the number of bears required to use the button in numPlushReq (number of plushies
+     * required).
+     * 
+     * Door MUST be a child of a cylindrical hinge.
+     */
+
+
+    public GameObject hinge;
     private bool pressed;
     public int numPlushReq;
     private int numPlush = 0;
@@ -38,7 +47,7 @@ public class ButtonController : MonoBehaviour
     {
         if (pressed && numPlush == numPlushReq)
         {
-            door.transform.localRotation = Quaternion.Slerp(door.transform.localRotation, Quaternion.Euler(0, 90, 0), Time.deltaTime * 2);
+            hinge.transform.localRotation = Quaternion.Slerp(hinge.transform.localRotation, Quaternion.Euler(0, 90, 0), Time.deltaTime * 2);
         }
     }
 }
