d2d8b8f90913f2f220e6f0a166544626cc90d55d
Scott Gelini <sg00251@srcs.org>
2020-02-05 13:44:06 -0800

    Follow done

--- Boardwalk Breakout/Assets/Meaghan Scripts/PlayerControllerMeg.cs
+++ Boardwalk Breakout/Assets/Meaghan Scripts/PlayerControllerMeg.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerControllerMeg : MonoBehaviour
 {
     public Vector3 jump;
     public float jumpForce = 2.0f;
