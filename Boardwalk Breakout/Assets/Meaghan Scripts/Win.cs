1028a862f232f4c43828c7ba4651889047225615
mb00126 <meaghanb101@gmail.com>
2020-03-04 14:59:41 -0800

    Grab + Endings
    also we lost nav mesh

---
+++ Boardwalk Breakout/Assets/Meaghan Scripts/Win.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Win : MonoBehaviour
+{
+    public GameObject win;
+    public GameObject wintext;
+    public bool isWin { get; private set; }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isWin = true;
+            win.SetActive(true);
+            wintext.SetActive(true);
+        }
+    }
+}
--- Boardwalk Breakout/Assets/Scripts/PlayerController.cs
+++ Boardwalk Breakout/Assets/Scripts/PlayerController.cs
@@ -15,6 +15,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        
     }
 
     void OnCollisionEnter(Collision collision)
@@ -33,25 +34,27 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
 
+        if (!GameObject.Find("Beam").GetComponent<GrabPlushie>().isDead && !GameObject.Find("Exit").GetComponent<Win>().isWin)
+        {
+            if (Input.GetKey("w"))
+            {
+                //pos.z += speed * Time.deltaTime;
+                rb.AddRelativeForce(Vector3.forward * speed);
+            }
+            if (Input.GetKey("s"))
+            {
+                //pos.z -= speed * Time.deltaTime;
 
-        if (Input.GetKey("w"))
-        {
-            //pos.z += speed * Time.deltaTime;
-            rb.AddRelativeForce(Vector3.forward * speed);
-        }
-        if (Input.GetKey("s"))
-        {
-            //pos.z -= speed * Time.deltaTime;
-
-            rb.AddRelativeForce(Vector3.forward * -speed);
-        }
-        if (Input.GetKey("d"))
-        {
-            this.transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey("a"))
-        {
-            this.transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
+                rb.AddRelativeForce(Vector3.forward * -speed);
+            }
+            if (Input.GetKey("d"))
+            {
+                this.transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+            }
+            if (Input.GetKey("a"))
+            {
+                this.transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
+            }
         }
         
     }
